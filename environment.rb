0a0265f1160888634b25e012c5d539334b9ec52d
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2012-05-30 23:51:45 +0200

    acceptable stylesheet for print

--- environment.rb
+++ environment.rb
@@ -8,12 +8,31 @@ require 'rubypants'
 require './extensions'
 require './page'
 
-GIT_REPO = ENV['HOME'] + '/wiki'
-HOMEPAGE = 'home'
 
-unless File.exists?(GIT_REPO) && File.directory?(GIT_REPO)
-  puts "Initializing repository in #{GIT_REPO}..."
-  Git.init(GIT_REPO)
+REPO_LOCATIONS = ["#{ENV['HOME']}/wiki", "#{ENV['HOME']}/.wiki"]
+
+def is_repo?(location)
+  File.exists?(location) && File.directory?(location)          
 end
 
+def find_repo(locations = REPO_LOCATIONS)
+  locations.detect do |location|
+    is_repo?(location)
+  end
+end
+
+def create_repo(location = REPO_LOCATIONS.first)
+  puts "Initializing repository in #{location}..."
+  Git.init(location)
+  location
+end
+
+def find_or_create_repo
+  find_repo || create_repo 
+end
+
+GIT_REPO = find_or_create_repo
+HOMEPAGE = 'home'
+
 $repo = Git.open(GIT_REPO)
+
